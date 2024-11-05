afcda3df2ced7d23c6b08be8f73ba9f72cbd0d34
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-11-05 19:54:33 +0100

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Yuri Petrosyan on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
